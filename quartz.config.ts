1cb209833845c2f590c67c34b5a6616c5fd1ecd9
mr-tinkle-winkle <blxckchxckxnz@gmail.com>
2026-04-14 18:24:43 -0500

    Quartz sync: Apr 14, 2026, 6:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Internal Affairs Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mr-tinkle-winkle.github.io/internal-affairs-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
